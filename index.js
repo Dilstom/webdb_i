08b06ece9fc251ab25b291d327e054d6d3e2067b
Dil Sombayeva <dilyasom@gmail.com>
2019-04-26 12:11:33 -0700

    require knex

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const express = require('express');
+
+const knex = require('knex');
+
+// const db = knex({
+//  client: 'sqlite3',
+//  connection: {
+//   filename: './db.sqlite3',
+//  },
+//  useNullAsDefault: true,
+// });
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development); // to interact with our db
+
 const server = express();
 
 server.use(express.json());
