5182e10599505375cece402315ebac0e366d6ebb
Dil Sombayeva <dilyasom@gmail.com>
2019-04-26 12:12:21 -0700

    put req

--- index.js
+++ index.js
@@ -31,5 +31,23 @@ server.post('/characters', (req, res) => {
   });
 });
 
+server.put('/characters/:id', (req, res) => {
+ const changes = req.body;
+ const { id } = req.params;
+ // there is no need to pass all the fields for the records, only the fields that need to change and
+ // knex will leave all other fields unchanged. Knex will do the right thing.
+ db('characters')
+  .where({ id: id }) // or .where('id', '=', id)
+  .update(changes)
+  // .update() method returns the number of records updated
+  .then(count => {
+   //count === number of records updated
+   res.status(200).json(count);
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
+
 
 server.listen(8000, () => console.log('Running on port 8000'));
